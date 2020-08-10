e3071d5f80b3d3ca73dbe6b131ede8d20cb627d1
Dennis Merkus <dennis.merkus@pm.me>
2020-08-10 13:45:55 +0200

    Add setup.py

---
+++ README.md
@@ -0,0 +1,2 @@
+# Omnilingual
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="omnilingual",
+    version="0.1.0",
+    author="Dennis Merkus",
+    description="Language code/feature utilities.",
+    long_description=long_description,
+    url="https://github.com/DennisMerkus/pyomnilingual",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering",
+    ],
+    python_requires=">=3.8",
+)
