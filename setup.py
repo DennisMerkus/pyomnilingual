b78b6eb0a583722044508e8c27d35cc7763f35d9
Dennis Merkus <dennis.merkus@pm.me>
2020-08-10 14:02:22 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name="omnilingual",
-    version="0.2.1",
+    version="0.3.0",
     author="Dennis Merkus",
     description="Language code/feature utilities.",
     long_description=long_description,
