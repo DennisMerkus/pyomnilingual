67e4e5c5eea0d50b9bc9c501a85dc9109ab2ac4f
Dennis Merkus <dennis.merkus@pm.me>
2020-08-10 14:02:08 +0200

    Add Banu features

---
+++ omnilingual/features/zbn/__init__.py
@@ -0,0 +1,25 @@
+from enum import Enum, unique
+
+
+@unique
+class BanuNounClass(Enum):
+    zwo = "zwo"
+    kto = "kto"
+    kso = "kso"
+    go = "go"
+    afa = "afa"
+    isi = "isi"
+    utu = "utu"
+    enge = "enge"
+    ndzo = "ndzo"
+
+
+@unique
+class BanuVerbType(Enum):
+    Type1 = 1
+    Type2 = 2
+    Type3 = 3
+    Type4 = 4
+    Type5 = 5
+    Type6 = 6
+    Type7 = 7
